5a733945bed5fd297f83661a8649b535fb4bbf54
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-03 00:21:38 -0700

    Allow local authorized keys to be provided to vcap user

---
+++ cookbooks/bosh_inception/attributes/default.rb
@@ -0,0 +1,2 @@
+# A String or Array of SSH public keys to populate the user's .ssh/authorized_keys file.
+default["user"]["authorized_keys"] = []
--- cookbooks/bosh_inception/recipes/vcap_user.rb
+++ cookbooks/bosh_inception/recipes/vcap_user.rb
@@ -4,7 +4,13 @@ user_account "vcap" do
   create_group true
 end
 
+user_account "vcap" do
+  ssh_keys node.user.authorized_keys
+  action :modify
+end
+
 sudo "vcap" do
   user      "%vcap"
   runas     "vcap"
 end
+
