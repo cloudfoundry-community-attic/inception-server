912ea672d4f588f0d29f7db2930b24c99800c89d
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-09 17:20:13 -0700

    unit spec for InceptionServerCookbook

--- lib/bosh/inception/inception_server_cookbook.rb
+++ lib/bosh/inception/inception_server_cookbook.rb
@@ -3,18 +3,25 @@ module Bosh::Inception
   class InceptionServerCookbook
     include FileUtils
 
-    attr_reader :server, :settings
+    attr_reader :server, :settings, :project_dir
 
-    def initialize(inception_server, settings)
+    class InvalidTarget < StandardError; end
+
+    def initialize(inception_server, settings, project_dir)
       @server = inception_server
       @settings = settings
+      @project_dir = project_dir
     end
 
+    # To be invoked within the settings_dir
     def converge
-      user_host = server.user_host
-      key_path = server.private_key_path
-      attributes = cookbook_attributes_for_inception.to_json
-      sh %Q{knife solo bootstrap #{user_host} -i #{key_path} -j '#{attributes}' -r 'bosh_inception'}
+      FileUtils.chdir(project_dir) do
+        raise InvalidTarget, "please invoke within folder containing nodes dir" unless File.directory?("nodes")
+        user_host = server.user_host
+        key_path = server.private_key_path
+        attributes = cookbook_attributes_for_inception.to_json
+        sh %Q{knife solo bootstrap #{user_host} -i #{key_path} -j '#{attributes}' -r 'bosh_inception'}
+      end
     end
 
     protected
