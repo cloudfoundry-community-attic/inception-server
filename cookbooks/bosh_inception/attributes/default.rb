ad892a19401e875ed33eef8073e29a1097e4ce82
Dr Nic Williams <drnicwilliams@gmail.com>
2013-09-03 16:21:54 -0700

    Setup node["user"]["home"] after defaulting node.user.username

--- cookbooks/bosh_inception/attributes/default.rb
+++ cookbooks/bosh_inception/attributes/default.rb
@@ -1,11 +1,11 @@
 # A String or Array of SSH public keys to populate the user"s .ssh/authorized_keys file.
 
-default["user"]["home"] = node.user.username == "root" ? "/root" : node["user"]["home"]
 default["disk"]["mounted"] = false
 default["disk"]["device"] = "/dev/xvdf"
 default["disk"]["fstype"] = "btrfs"
 default["disk"]["dir"] = "/var/vcap/store"
 default["user"]["username"] = `users | head -n 1`.strip
+default["user"]["home"] = node.user.username == "root" ? "/root" : "/home/#{node["user"]["username"]}"
 default["git"]["name"] = "Nobody"
 default["git"]["email"] = "nobody@in-the-house.com"
 default["rvm"]["default_ruby"] = "ruby-1.9.3"
